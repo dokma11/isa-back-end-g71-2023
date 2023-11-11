780e3e5e7ccdd127f2fb7420ec625884ab1b1afb
dokma11 <vule.dok@gmail.com>
2023-11-11 16:53:02 +0100

    feat: Company model, dto, controller, service and repo creation

---
+++ jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/dto/CompanyDTO.java
@@ -0,0 +1,83 @@
+package rs.ac.uns.ftn.informatika.jpa.dto;
+
+import rs.ac.uns.ftn.informatika.jpa.model.Company;
+
+public class CompanyDTO {
+    private Integer id;
+    private String name;
+    private String address;
+    private double longitude;
+    private double latitude;
+    private String description;
+    private double averageGrade;
+
+    public CompanyDTO(){
+        
+    }
+
+    public CompanyDTO(Company company){
+        this.id = company.getId();
+        this.name = company.getName();
+        this.address = company.getAddress();
+        this.longitude = company.getLongitude();
+        this.latitude = company.getLatitude();
+        this.description = company.getDescription();
+        this.averageGrade = company.getAverageGrade();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+}
---
+++ jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/model/Company.java
@@ -0,0 +1,134 @@
+package rs.ac.uns.ftn.informatika.jpa.model;
+
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address; // for this one need to add maps to save its location...
+
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "averageGrade", nullable = true)
+    private double averageGrade;
+
+/*
+    @OneToMany(mappedBy = "company", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<User> administrators = new List<User>(); // for now I will leave it as User, we need to see how we will model that
+    */
+
+    public Company() {
+    }
+
+    public Company(String name, String address, double longitude, double latitude, String description, double averageGrade) {
+        this.name = name;
+        this.address = address;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.description = description;
+        this.averageGrade = averageGrade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+/*
+    public List<User> getAdministrators() {
+        return administrators;
+    }
+
+    public void setAdministrators(List<User> administrators) {
+        this.administrators = administrators;
+    }
+
+     */
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
