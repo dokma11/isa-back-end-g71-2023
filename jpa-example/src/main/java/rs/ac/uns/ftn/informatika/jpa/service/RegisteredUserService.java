49302003d6d7e19f9a9307dbc395f43eca6b2d30
Jelena Kovač <jelena.kovac765@gmail.com>
2023-11-16 18:18:57 +0100

    feat: user registration- sending mail

--- jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/service/RegisteredUserService.java
+++ jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/service/RegisteredUserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.query.JpaEntityGraph;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Service;
 import rs.ac.uns.ftn.informatika.jpa.model.RegisteredUser;
 import rs.ac.uns.ftn.informatika.jpa.repository.RegisteredUserRepository;
@@ -16,8 +17,24 @@ public class RegisteredUserService {
     @Autowired
     RegisteredUserRepository registeredUserRepository;
 
+    @Autowired
+    MailService mailService;
+
     public Page<RegisteredUser> findAll(Pageable pageable) {return registeredUserRepository.findAll(pageable);}
-    public RegisteredUser create(RegisteredUser user) {return registeredUserRepository.save(user);}
+    public RegisteredUser create(RegisteredUser user) throws MailException {
+
+        RegisteredUser newUser =  registeredUserRepository.save(user);
+        try{
+                mailService.sendRegistrationNotification(newUser);
+        }
+        catch(InterruptedException e){
+            System.out.println("Interrupted exeption cught");
+        }
+
+
+        return newUser;
+
+    }
     public void remove(int id) { registeredUserRepository.deleteById(id);}
 
     public List<RegisteredUser> getAll() {return registeredUserRepository.findAll();}
