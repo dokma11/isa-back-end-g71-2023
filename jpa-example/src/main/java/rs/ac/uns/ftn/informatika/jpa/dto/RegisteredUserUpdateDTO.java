a2b3add77b230fa973f26c9e32f89cf9614e3a8e
kuzminacc <kuzminacn@gmail.com>
2023-11-16 16:48:46 +0100

    feat: registered user profile update

---
+++ jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/dto/RegisteredUserUpdateDTO.java
@@ -0,0 +1,95 @@
+package rs.ac.uns.ftn.informatika.jpa.dto;
+
+import rs.ac.uns.ftn.informatika.jpa.model.RegisteredUser;
+
+public class RegisteredUserUpdateDTO {
+
+    private String name;
+    private String surname;
+    private String password;
+    private String telephoneNumber;
+    private String city;
+    private String state;
+    private String profession;
+    private String companyInformation;
+
+    public String getName() {
+        return name;
+    }
+
+    public RegisteredUserUpdateDTO() {
+    }
+
+    public RegisteredUserUpdateDTO(RegisteredUser user){
+        name = user.getName();
+        surname = user.getSurname();
+        telephoneNumber = user.getTelephoneNumber();
+        city = user.getCity();
+        state = user.getState();
+        profession = user.getProfession();
+        password = user.getPassword();
+        companyInformation = user.getCompanyInformation();
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCompanyInformation() {
+        return companyInformation;
+    }
+
+    public void setCompanyInformation(String companyInformation) {
+        this.companyInformation = companyInformation;
+    }
+
+}
